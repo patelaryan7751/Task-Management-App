d69a09e89b24dff4022edfc77436d8fa77190228
ARYAN PATEL <patelaryan7751@gmail.com>
2022-02-27 05:05:50 +0530

    Update settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-22r3o1=qxv0_)4q#f=op=y79sc793o0n1w8%^-t_o4gp$+%h5j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["task-management-app7751.herokuapp.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
